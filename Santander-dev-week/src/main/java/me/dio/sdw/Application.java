c2b109ebdc0abd6ba0b3cb1d40c36827586fb329
Fernando <silentguyband@gmail.com>
2025-02-10 15:01:38 -0300

    Configuração do banco de dados

--- Santander-dev-week/src/main/java/me/dio/sdw/Application.java
+++ Santander-dev-week/src/main/java/me/dio/sdw/Application.java
@@ -1,13 +1,13 @@
-package me.dio.Sdw;
+package me.dio.sdw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SantanderDevWeekApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SantanderDevWeekApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }

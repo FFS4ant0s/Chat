b261aaebec918467fb3e4cd2edfed1e9ef502de0
Fernando <silentguyband@gmail.com>
2025-02-11 13:29:47 -0300

    Interface

--- Santander-dev-week/src/main/java/me/dio/sdw/ports/ChampionsRepository.java
+++ Santander-dev-week/src/main/java/me/dio/sdw/ports/ChampionsRepository.java
@@ -1,9 +1,12 @@
 package me.dio.sdw.ports;
 
 import java.util.List;
+import java.util.Optional;
 
 import me.dio.sdw.models.Champions;
 
 public interface ChampionsRepository {
     List<Champions> findall();
+
+    Optional<Champions> findById(Long id);
 }

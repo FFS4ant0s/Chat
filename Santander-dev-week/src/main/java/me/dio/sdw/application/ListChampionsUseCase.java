e7d3ed2114adebae1fb4eced30b4811058a4908f
Fernando <silentguyband@gmail.com>
2025-02-11 14:13:19 -0300

    List Champions

--- Santander-dev-week/src/main/java/me/dio/sdw/application/ListChampionsUseCase.java
+++ Santander-dev-week/src/main/java/me/dio/sdw/application/ListChampionsUseCase.java
@@ -1,7 +1,14 @@
 package me.dio.sdw.application;
 
+import java.util.List;
+
+import me.dio.sdw.models.Champions;
 import me.dio.sdw.ports.ChampionsRepository;
 
 public record ListChampionsUseCase (ChampionsRepository repository) {
+
+    public List<Champions> findAll(){
+        return  repository.findAll();
+    }
 
 }
